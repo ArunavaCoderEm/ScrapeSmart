2916cd96d58c12c00bf7d17aa17dd8f92fdc1579
ArunavaCoderEm <acecankill@gmail.com>
2024-10-12 22:51:54 +0530

    noce v04

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 import streamlit as st
 
-from scrapping import scrape_website
+from scrapping import (
+    remove_unnecessery,
+    clean_from_body,
+    split_dom_content,
+    scrape_website
+)
 
 st.title("ScrapeSmart")
 scrappable_url = st.text_input("Enter an URL here - ")
 
 if (st.button("Get Data")):
 
-    st.write("Working")
+    st.write("Working ...")
     
     data = scrape_website(scrappable_url)
-    
-    print(data)
-    
+    body_content = remove_unnecessery(data)
+    cleaned_content = clean_from_body(body_content)
+
+    st.session_state.dom_content = cleaned_content
+
+
+    with st.expander("View Website Content"):
+        st.text_area("Website Content", cleaned_content, height=300)
+
--- scrapping.py
+++ scrapping.py
@@ -1,6 +1,8 @@
 from selenium.webdriver import Remote, ChromeOptions
 from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
 
+from bs4 import BeautifulSoup
+
 SBR_WEBDRIVER = 'https://brd-customer-hl_a101e284-zone-scrapesmart:uz47tjb0bcem@brd.superproxy.io:9515'
 
 def scrape_website(url):
@@ -30,3 +32,36 @@ def scrape_website(url):
     finally :
         
         print("Scrapping done most probably")
+
+
+def remove_unnecessery (htmlcode):
+    
+    bs = BeautifulSoup(htmlcode, "html.parser")
+    body = bs.body
+    
+    if (body) :
+         return str(body)
+     
+    else : 
+        return ""
+    
+def clean_from_body(body):
+    
+    bs = BeautifulSoup(body, "html.parser")
+    
+    for i in bs(["style", "script"]):
+        
+        i.extract()
+        
+    cleaned = bs.get_text(separator="\n")
+    
+    cleaned_content = "\n".join(
+        line.strip() for line in cleaned.splitlines() if line.strip()
+    )
+    
+    return cleaned_content
+
+def split_dom_content(dom_content, max_length=6000):
+    return [
+        dom_content[i : i + max_length] for i in range(0, len(dom_content), max_length)
+    ]
