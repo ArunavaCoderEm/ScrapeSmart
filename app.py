291c985dfd05f529961b8bcb96dcf9b8fc3faa67
ArunavaCoderEm <acecankill@gmail.com>
2024-10-12 21:47:03 +0530

    ver v02

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import streamlit as st
 
+from scrapping import scrape_website
+
 st.title("ScrapeSmart")
 scrappable_url = st.text_input("Enter an URL here - ")
 
 if (st.button("Get Data")):
+
     st.write("Working")
+    
+    data = scrape_website(scrappable_url)
+    
+    print(data)
     
--- scrapping.py
+++ scrapping.py
@@ -2,25 +2,34 @@ import selenium.webdriver as driver
 import selenium.webdriver.chrome.service as srv
 import time
 
-def scrape_website (url):
+def scrape_website(url):
     
-    path = "./chromedriver"
+    path = "./chromedriver.exe"
+
+    
     options = driver.ChromeOptions()
+
     
-    drv = driver.Chrome(service = srv(path), options = options)
+    chrome_service = srv.Service(executable_path=path)
+
     
+    drv = driver.Chrome(service=chrome_service, options=options)
+
     try :
-        
+       
         drv.get(url)
-        codedSite = drv.page_source
+
         time.sleep(15)
-        
+
+        codedSite = drv.page_source
+
         return codedSite
-        
-    except :
-        
-        print("Some error occured")
-        
+
+    except Exception as e :
+
+        print(f"Some error occurred: {e}")
+
     finally :
-        
+
         drv.quit()
+
